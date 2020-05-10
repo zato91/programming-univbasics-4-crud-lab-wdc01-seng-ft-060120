9da32f7973d7507e708443b00a34fc362ffac28d
zara ahmadi <zatokey@gmail.com>
2020-05-10 00:37:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,25 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
-  
+  array =['prout','prite','pouf']
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shit
 end
 
 def retrieve_element_from_index(array, index_number)
