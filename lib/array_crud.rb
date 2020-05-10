0daad1af422612fd0930c17b2ed04f4a5f6a86f0
zara ahmadi <zatokey@gmail.com>
2020-05-10 00:39:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,17 +23,17 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array.length -1
 end
 
 def update_element_from_index(array, index_number, element)
-
+array[index_number] = element
 end
